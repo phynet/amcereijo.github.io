150305844297d3c87b3aec37bb99a5d392493d3e
amcereijo <amcereijo@gmail.com>
2015-03-15 19:54:18 +0100

    utilities function "randomColor"

--- index.js
+++ index.js
@@ -16,6 +16,24 @@
 }(jQuery));
 
 var app = {};
+app.util = (function(){
+	var colors = [],
+		randomColor = function() {
+			var letters = '0123456789ABCDEF'.split(''),
+				color = '#',
+				i;
+			do {
+		    	for (i = 0; i < 6; i++ ) {
+		        	color += letters[Math.floor(Math.random() * 16)];
+		    	}
+		    } while(colors.indexOf(color) !== -1);
+		    colors.push(color);
+		    return color
+		};
+	return {
+		randomColor: randomColor
+	}
+})();
 
 $(document).ready(function() {
 	console.log('ready!!');
